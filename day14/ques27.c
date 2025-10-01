70c9f8a307ea524eec7309b7bd4e437595284586
Vasvi Sood <vasvisood2007@gmail.com>
2025-10-01 17:30:54 +0530

    Add files via upload

--- day14/ques27.c
+++ day14/ques27.c
@@ -1,1 +1,34 @@
+//Q27: Write a program to print the sum of the first n odd numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+3
+Output 1:
+9
+
+Input 2:
+5
+Output 2:
+25
+
+*/
+#include<stdio.h>
+int main()
+{
+    int n;
+    int sum=0;
+    printf("Enter the limit");
+    scanf("%d",&n);
+    for(int i=1;i<=2*n;i+=2)
+    {
+      
+      sum=sum+i;
+    }
+    printf("Sum=%d", sum);
+    return 0;
+
+    
+}
+
 
