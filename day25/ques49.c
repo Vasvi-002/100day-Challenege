ab173d1d2393336597de680277c96dc6121e332f
Vasvi Sood <vasvisood2007@gmail.com>
2025-10-01 17:51:35 +0530

    Add files via upload

--- day25/ques49.c
+++ day25/ques49.c
@@ -1,1 +1,35 @@
+//Q49: Write a program to print the following pattern:
+//5
+//45
+//345
+//2345
+//12345
 
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+5
+45
+345
+2345
+12345
+
+*/
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int n = 5; 
+
+    for (i = 1; i <= n; i++) {
+        
+        for (j = n - i + 1; j <= n; j++) {
+            printf("%d", j);
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
