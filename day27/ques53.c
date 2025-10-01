2b6cb393deb7130d032bf505e02094f8109b89b9
Vasvi Sood <vasvisood2007@gmail.com>
2025-10-01 17:56:21 +0530

    Add files via upload

--- day27/ques53.c
+++ day27/ques53.c
@@ -1,1 +1,50 @@
+//Q53: Write a program to print the following pattern:
+//*
+//***
+//*****
+//*******
+//*********
+//*******
+//*****
+//***
+//*
 
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+*/
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int n = 5; 
+
+    for (i = 1; i <= n; i += 2) {
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    
+    for (i = n-2; i >= 1; i -= 2) {
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
