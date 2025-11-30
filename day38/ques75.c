08c4e3fe823c4774aec35ed8d57caa3ed88edb6f
Vasvi Sood <vasvisood2007@gmail.com>
2025-11-30 19:15:53 +0530

    Add files via upload

--- day38/ques75.c
+++ day38/ques75.c
@@ -1,1 +1,62 @@
+//Q75: Add two matrices.
 
+/*
+Sample Test Cases:
+Input 1:
+2 2
+1 2
+3 4
+2 2
+5 6
+7 8
+Output 1:
+6 8
+10 12
+
+*/
+#include <stdio.h>
+
+int main() {
+   
+    int rows, cols, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+    int matrix1[rows][cols], matrix2[rows][cols], sum[rows][cols];
+    
+    printf("Enter elements of first matrix:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            printf("matrix1[%d][%d]: ", i, j);
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    
+    printf("Enter elements of second matrix:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            printf("matrix2[%d][%d]: ", i, j);
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    printf("\nSum of the two matrices:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
