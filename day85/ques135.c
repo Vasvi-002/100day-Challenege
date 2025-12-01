343bd36609cc86c365d80a35571ccfe98c02dbb6
Vasvi Sood <vasvisood2007@gmail.com>
2025-12-01 18:53:36 +0530

    Add files via upload

--- day85/ques135.c
+++ day85/ques135.c
@@ -1,1 +1,27 @@
+//Q135: Assign explicit values starting from 10 and print them.
 
+/*
+Sample Test Cases:
+Input 1:
+No input
+Output 1:
+A=10
+B=11
+C=12
+
+*/
+#include <stdio.h>
+
+enum Status {
+    SUCCESS = 10,
+    FAILURE,
+    TIMEOUT
+};
+
+int main() {
+    printf("SUCCESS = %d\n", SUCCESS);
+    printf("FAILURE = %d\n", FAILURE);
+    printf("TIMEOUT = %d\n", TIMEOUT);
+    
+    return 0;
+}
