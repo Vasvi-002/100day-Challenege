68b32ac2ac6df37c340929d0b1607bf61b74fa9e
Vasvi Sood <vasvisood2007@gmail.com>
2025-12-01 18:55:39 +0530

    Add files via upload

--- day88/ques138.c
+++ day88/ques138.c
@@ -1,1 +1,30 @@
+//Q138: Print all enum names and integer values using a loop.
 
+/*
+Sample Test Cases:
+Input 1:
+No input
+Output 1:
+RED=0
+YELLOW=1
+GREEN=2
+
+*/
+#include <stdio.h>
+
+enum Role {
+    ADMIN = 1,
+    USER,
+    GUEST
+};
+
+int main() {
+
+    const char *roleNames[] = { "ADMIN", "USER", "GUEST" };
+
+    for (int i = ADMIN; i <= GUEST; i++) {
+        printf("%s = %d\n", roleNames[i - 1], i);
+    }
+
+    return 0;
+}
