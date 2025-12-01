b3f8e4ef1e72e877cfe19b0fd530e85273960198
Vasvi Sood <vasvisood2007@gmail.com>
2025-12-01 18:56:24 +0530

    Add files via upload

--- day89/ques139.c
+++ day89/ques139.c
@@ -1,1 +1,31 @@
+//Q139: Show that enums store integers by printing assigned values.
 
+/*
+Sample Test Cases:
+Input 1:
+No input
+Output 1:
+SUCCESS=0, FAILURE=1, TIMEOUT=2
+
+*/
+#include <stdio.h>
+
+enum Example {
+    A,       
+    B,       
+    C = 10,   
+    D,        
+    E = 5,    
+    F         
+};
+
+int main() {
+    printf("A = %d\n", A);
+    printf("B = %d\n", B);
+    printf("C = %d\n", C);
+    printf("D = %d\n", D);
+    printf("E = %d\n", E);
+    printf("F = %d\n", F);
+
+    return 0;
+}
