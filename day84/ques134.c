7b6bc0d20182c837859ae8e8c522fa6200435186
Vasvi Sood <vasvisood2007@gmail.com>
2025-12-01 18:52:54 +0530

    Add files via upload

--- day84/ques134.c
+++ day84/ques134.c
@@ -1,1 +1,37 @@
+//Q134: Define an enum with SUCCESS, FAILURE, and TIMEOUT, and print messages accordingly.
 
+/*
+Sample Test Cases:
+Input 1:
+FAILURE
+Output 1:
+Operation failed
+
+*/
+#include <stdio.h>
+
+enum Status {
+    SUCCESS,
+    FAILURE,
+    TIMEOUT
+};
+
+int main() {
+    enum Status s = TIMEOUT;   
+
+    switch (s) {
+        case SUCCESS:
+            printf("Operation completed successfully.\n");
+            break;
+        case FAILURE:
+            printf("Operation failed.\n");
+            break;
+        case TIMEOUT:
+            printf("Operation timed out.\n");
+            break;
+        default:
+            printf("Unknown status.\n");
+    }
+
+    return 0;
+}
