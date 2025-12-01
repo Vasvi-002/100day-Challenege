8e3bb996e7f002a4514766272139c41b42b95c6a
Vasvi Sood <vasvisood2007@gmail.com>
2025-12-01 18:58:25 +0530

    Add files via upload

--- day92/ques142.c
+++ day92/ques142.c
@@ -1,1 +1,45 @@
+//Q142: Store details of 5 students in an array of structures and print all.
 
+/*
+Sample Test Cases:
+Input 1:
+Details of 5 students (Name, Roll, Marks)
+Output 1:
+Tabular list of all 5 students with their details
+
+*/
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+int main() {
+    struct Student s[5];
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        printf("\nEnter details for student %d:\n", i + 1);
+
+        printf("Name: ");
+        scanf("%s", s[i].name);
+
+        printf("Roll No: ");
+        scanf("%d", &s[i].roll_no);
+
+        printf("Marks: ");
+        scanf("%f", &s[i].marks);
+    }
+
+    printf("\n----- Student Details -----\n");
+    for (i = 0; i < 5; i++) {
+        printf("\nStudent %d:\n", i + 1);
+        printf("Name     : %s\n", s[i].name);
+        printf("Roll No  : %d\n", s[i].roll_no);
+        printf("Marks    : %.2f\n", s[i].marks);
+    }
+
+    return 0;
+}
