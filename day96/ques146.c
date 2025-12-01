799fa4e1e70941b23df2032c3743666ef83f6de9
Vasvi Sood <vasvisood2007@gmail.com>
2025-12-01 19:01:18 +0530

    Add files via upload

--- day96/ques146.c
+++ day96/ques146.c
@@ -1,1 +1,43 @@
+//Q146: Create Employee structure with nested Date structure for joining date and print details.
 
+/*
+Sample Test Cases:
+Input 1:
+Employee: Raj | ID: 11 | Joining Date: 12 05 2020
+Output 1:
+Name: Raj | ID: 11 | Joining Date: 12/05/2020
+
+*/
+#include <stdio.h>
+
+struct Date {
+    int day;
+    int month;
+    int year;
+};
+
+struct Employee {
+    char name[50];
+    int id;
+    struct Date join;
+};
+
+int main() {
+    struct Employee e;
+
+    printf("Enter name: ");
+    scanf("%s", e.name);
+
+    printf("Enter ID: ");
+    scanf("%d", &e.id);
+
+    printf("Enter joining date (dd mm yyyy): ");
+    scanf("%d %d %d", &e.join.day, &e.join.month, &e.join.year);
+
+    printf("\n--- Employee Details ---\n");
+    printf("Name : %s\n", e.name);
+    printf("ID   : %d\n", e.id);
+    printf("Joining Date : %d-%d-%d\n", e.join.day, e.join.month, e.join.year);
+
+    return 0;
+}
